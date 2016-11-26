1a170b4d3f2dae5694468d71e4b40ea02c4cb87d
Dende87 <dende76@gmx.de>
2016-11-26 19:01:10 +0100

    adding for loops

--- src/ue1Java5/AutoboxingUnboxing.java
+++ src/ue1Java5/AutoboxingUnboxing.java
@@ -20,5 +20,21 @@ public class AutoboxingUnboxing {
         }
 
         System.out.println("Sum = " + sum);
+
+        int[] numbers = {3,5,7,-2,4};
+
+        // normal for-loop
+        sum = 0;
+        for (int i = 0; i < numbers.length;i++){
+            sum += numbers[i];
+        }
+        System.out.println("Sum = " + sum);
+
+        // forEach loop
+        sum = 0;
+        for (int num: numbers) {
+            sum += num;
+        }
+        System.out.println("Sum = " + sum);
     }
 }
