4b21155ac7a79a25ebc14146e9dc9d5d9e4d1236
Dende87 <dende76@gmx.de>
2016-12-16 19:18:45 +0100

    create Attikus class

--- src/model/Attikus.java
+++ src/model/Attikus.java
@@ -4,4 +4,25 @@ package model;
  * Created by Dende on 03.12.2016.
  */
 public class Attikus {
+
+    private Territory territory;
+    private final TileType attikusTileType = TileType.ATTIKUS;
+    private Position position;
+
+    //Attikus kennt sein Spielfeld
+    public Attikus(Territory territory, Position position) {
+        this.position = position;
+        this.territory = territory;
+    }
+
+    //wo befindet sich Attikus
+    public Position getAttikusPosition(){
+        return position;
+    }
+
+    //setze Attikus auf Spielfeld
+    public void setAttikusOnTerritory(Position position){
+        this.position = position;
+        territory.changeTileType(position, attikusTileType);
+    }
 }
