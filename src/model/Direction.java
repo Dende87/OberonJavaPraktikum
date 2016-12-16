4f145349b9b9f658220978035323190299b9a7ca
Dende87 <dende76@gmx.de>
2016-12-16 19:20:55 +0100

    create Attikus class

--- src/model/Direction.java
+++ src/model/Direction.java
@@ -9,16 +9,4 @@ public enum Direction {
     EAST,
     SOUTH,
     WEST
-
-    //iteration durch Enums Quelle: http://stackoverflow.com/questions/17664445/is-there-an-increment-operator-for-java-enum
-            /*{
-        @Override
-        public Direction next() {
-            return null; // see below for options for this line
-        }
-    };
-    public Direction next() {
-        // No bounds checking required here, because the last instance overrides
-        return values()[ordinal() + 1];
-    }*/
 }
